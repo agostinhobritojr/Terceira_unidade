4b802d9e75a38591a0105adabc97b98a271e618f
EveertonCordeiro12 <79225781+EveertonCordeiro12@users.noreply.github.com>
2021-04-30 18:54:07 -0300

    Create figurageometrica.h

---
+++ figurageometrica.h
@@ -0,0 +1,17 @@
+# Ifndef FIGURAGEOMETRICA_H
+# define  FIGURAGEOMETRICA_H
+# inclui  " escultor.h "
+# inclui  < iostream >
+
+usando  namespace  std ;
+
+classe  FiguraGeometrica
+{
+público:
+    FiguraGeometrica ();
+     sorteio de vazio  virtual (Escultor & t) = 0;
+    virtual   ~ FiguraGeometrica ();
+
+};
+
+# endif  // FIGURAGEOMETRICA_H
